27536fb6a524db78a687792190885ee8aab80a73
Everlag <kyleveb@gmail.com>
2017-02-20 18:53:21 -0700

    cmd check command for local cache

--- cmd/commands.go
+++ cmd/commands.go
@@ -32,6 +32,21 @@ var fetchCmd = &cobra.Command{
 	},
 }
 
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "check if cached stash update is valid",
+	Long:  "get the stash update from disk and try to deserialize",
+	Run: func(cmd *cobra.Command, args []string) {
+		resp, err := stash.GetStored()
+		if err != nil {
+			fmt.Printf("failed to read cached stash data, err=%s\n", err)
+			os.Exit(-1)
+		}
+		fmt.Printf("read cached stash update, %d entries found\n", len(resp.Stashes))
+	},
+}
+
 func init() {
 	RootCmd.AddCommand(fetchCmd)
+	RootCmd.AddCommand(checkCmd)
 }
