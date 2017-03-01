f19d2e5d7a9fcc82974ba2a04fdff82089929edb
Everlag <kyleveb@gmail.com>
2017-03-01 16:57:42 -0700

    db begin introducing IndexQuery with associated context

--- db/index.go
+++ db/index.go
@@ -269,6 +269,165 @@ func LookupItems(rootType, rootFlavor, mod StringHeapID,
 	return ids, err
 }
 
+// IndexQuery represents a query running over established indices
+//
+// An IndexQuery can be rerun by reinitializing the ctx; this typically
+// happens when the query is Run.
+type IndexQuery struct {
+	// Type and flavor of the item we're looking up
+	rootType, rootFlavor StringHeapID
+	// Mods we are looking for
+	mods []StringHeapID
+	// Minimum mod values we are required to find
+	//
+	// Positionally related to mods
+	minModValues []uint16
+	// League we are searching for
+	league LeagueHeapID
+	// How many items we are limited to finding
+	maxDesired int
+	// Context necessary for a query to run
+	ctx *indexQueryContext
+}
+
+// indexQueryContext represents the necessary transaction-dependent
+// context for an IndexQuery to run.
+type indexQueryContext struct {
+	tx           *bolt.Tx
+	validCursors int
+	// Cursors we iterate over to perform our query
+	//
+	// These are positionally related to the parent's IndexQuery.mods
+	cursors []*bolt.Cursor
+	sets    []map[ID]struct{}
+}
+
+// NewIndexQuery returns an IndexQuery with no context
+func NewIndexQuery(rootType, rootFlavor StringHeapID,
+	mods []StringHeapID, minModValues []uint16,
+	league LeagueHeapID,
+	maxDesired int) IndexQuery {
+
+	return IndexQuery{
+		rootType, rootFlavor,
+		mods, minModValues,
+		league, maxDesired, nil,
+	}
+
+}
+
+// initContext prepares transaction dependent context for an IndexQuery
+func (q *IndexQuery) initContext(tx *bolt.Tx) error {
+
+	// Make a place to keep our cursors
+	//
+	// NOTE: a cursor can be nil to indicate it should not be queried
+	cursors := make([]*bolt.Cursor, len(q.mods))
+
+	// Keep track of how many cursors are valid,
+	// this will let us know when we've exhausted our data
+	validCursors := len(cursors)
+
+	// Collect our buckets for each mod and establish cursors
+	for i, mod := range q.mods {
+		itemModBucket, err := getItemModIndexBucketRO(q.rootType, q.rootFlavor,
+			mod, q.league, tx)
+		if err != nil {
+			return fmt.Errorf("faield to get item mod index bucket, mod=%d err=%s",
+				mod, err)
+		}
+		cursors[i] = itemModBucket.Cursor()
+	}
+
+	// Create our item sets
+	sets := make([]map[ID]struct{}, len(q.mods))
+	for i := range sets {
+		sets[i] = make(map[ID]struct{})
+	}
+
+	q.ctx = &indexQueryContext{
+		tx, validCursors, cursors, sets,
+	}
+
+	return nil
+}
+
+// Run initialises transaction context for a query and attempts
+// to find desired items.
+func (q *IndexQuery) Run(db *bolt.DB) ([]ID, error) {
+
+	// ids presized...
+	var ids []ID
+
+	err := db.View(func(tx *bolt.Tx) error {
+
+		err := q.initContext(tx)
+		if err != nil {
+			return fmt.Errorf("failed to initialize query context")
+		}
+
+		// Set all of our cursors to be at their ends
+		for i, c := range q.ctx.cursors {
+			// Set to last
+			k, v := c.Last()
+			// Ignore nested buckets
+			if k == nil {
+				continue
+			}
+			// Grab the value
+			values, err := decodeModIndexKey(k)
+			if err != nil {
+				return fmt.Errorf("failed to decode mod index key, err=%s", err)
+			}
+			if len(values) == 0 {
+				return fmt.Errorf("decoded item mod index key to no values, key=%v", k)
+			}
+
+			// Ensure the mod is the correct value
+			if values[0] >= q.minModValues[i] {
+				if len(v) != IDSize {
+					panic(fmt.Sprintf("malformed id value in index, incorrect length; id=%v", v))
+				}
+				// NOTE: the copy here is actually completely required
+				// due to the fact that boltdb makes no guarantee regarding what
+				// keys and value slices contain when outside a transaction.
+				var id ID
+				copy(id[:], v)
+				q.ctx.sets[i][id] = struct{}{}
+			} else {
+				// Remove from cursors we're interested in
+				q.ctx.cursors[i] = nil
+				q.ctx.validCursors--
+			}
+		}
+
+		// Perform our strides to search
+		var foundIDs int
+		for foundIDs < q.maxDesired && q.ctx.validCursors > 0 {
+			// Iterate for a stride
+			err := lookupMultiModStride(q.minModValues,
+				q.ctx.sets, q.ctx.cursors, &q.ctx.validCursors)
+			if err != nil {
+				return fmt.Errorf("failed a stride, err=%s", err)
+			}
+
+			foundIDs = intersectItemIDSets(q.ctx.sets, nil)
+		}
+
+		// Cap result to desired length as required
+		if foundIDs > q.maxDesired {
+			foundIDs = q.maxDesired
+		}
+		// Perform one more intersection to find our return value
+		ids = make([]ID, foundIDs)
+		intersectItemIDSets(q.ctx.sets, ids)
+
+		return nil
+	})
+
+	return ids, err
+}
+
 // LookupItemsMultiModStrideLength determines how many items
 // is included in a stride of LookupItemsMultiMod.
 //
