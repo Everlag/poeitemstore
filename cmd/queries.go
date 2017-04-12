9ed4f41b9ddc24d8da0cb4bf4306e7916e67f357
Everlag <kyleveb@gmail.com>
2017-04-11 18:54:36 -0600

    cmd MultiModSearch pretty print with String

--- cmd/queries.go
+++ cmd/queries.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // MultiModSearch specifies a search to perform for items
@@ -14,6 +15,27 @@ type MultiModSearch struct {
 	League     string
 	Mods       []string
 	MinValues  []uint16
+}
+
+func (search *MultiModSearch) String() string {
+	modPrints := make([]string, len(search.Mods))
+	var modString string
+	if len(search.Mods) != len(search.MinValues) {
+		modString = "invalid mods: len(Mods) != len(MinValues)"
+	} else {
+		for i, mod := range search.Mods {
+			modPrints[i] = fmt.Sprintf("%s: %d", mod, search.MinValues[i])
+		}
+		if len(modPrints) == 0 {
+			modString = "no mods present"
+		}
+		modString = strings.Join(modPrints, "\n")
+	}
+	return fmt.Sprintf(`RootType: %s, RootFlavor: %s,
+League: %s, MaxDesired: %d
+%s`,
+		search.RootType, search.RootFlavor,
+		search.League, search.MaxDesired, modString)
 }
 
 // FetchMultiModSearch returns a MultiModSearch deserialized
