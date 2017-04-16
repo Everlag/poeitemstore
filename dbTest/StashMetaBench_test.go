e1214f461235187648ea9785cf98143a910467a9
Everlag <kyleveb@gmail.com>
2017-04-16 16:51:23 -0600

    dbTest BenchmarkAddStashes introduce
    How long it takes to add stashes is an important metric that could
    offset performance gains from query optimization.

---
+++ dbTest/StashMetaBench_test.go
@@ -0,0 +1,46 @@
+package dbTest
+
+import (
+	"testing"
+
+	"github.com/Everlag/poeitemstore/db"
+)
+
+// BenchmarkAddStashes determines how fast adding a single
+// response to the database is.
+func BenchmarkAddStashes(b *testing.B) {
+
+	// Fetch the changes we need
+	set := GetChangeSet("testSet - 11 updates.msgp", b)
+	change, err := set.GetFirstChange()
+	if err != nil {
+		b.Fatalf("failed to get first change")
+	}
+
+	resp, err := change.Decompress()
+	if err != nil {
+		b.Fatalf("failed to decompress response")
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		// Create a database, we want to exclude
+		// the time this takes from the benchmark
+		b.StopTimer()
+		bdb := NewTempDatabase(b)
+		b.StartTimer()
+
+		cStashes, cItems, err := db.StashStashToCompact(resp.Stashes, bdb)
+		if err != nil {
+			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
+		}
+
+		_, err = db.AddStashes(cStashes, cItems, bdb)
+		if err != nil {
+			b.Fatalf("failed to AddStashes, err=%s", err)
+		}
+	}
+
+}
