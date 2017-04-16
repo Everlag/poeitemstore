07a12008aab1a68d44f507e00cacb27187c4823c
Everlag <kyleveb@gmail.com>
2017-04-16 15:37:18 -0600

    db IndexQuery increase StrideLength to 32 from 1
    This causes more allocations but it runs significantly faster.
    
                                          old ns/op     new ns/op     delta
    SingleIndexQueryFast-4                208635        65516
    -68.60%
    FiveIndexQueryFast-4                  4300890       591592
    -86.24%
    InterleavedLeagueIndexQueryFast-4     5466756       877993
    -83.94%
    
                                          old allocs     new allocs
    delta
    SingleIndexQueryFast-4                259            315
    +21.62%
    FiveIndexQueryFast-4                  2142           2310
    +7.84%
    InterleavedLeagueIndexQueryFast-4     3348           3696
    +10.39%
    
                                          old bytes     new bytes     delta
    SingleIndexQueryFast-4                10299         10715         +4.04%
    FiveIndexQueryFast-4                  84901         87061         +2.54%
    InterleavedLeagueIndexQueryFast-4     137326        144018        +4.87%

--- db/IndexQuery.go
+++ db/IndexQuery.go
@@ -11,7 +11,7 @@ import (
 //
 // Longer strides mean fewer intersections but more potentially useless
 // item mods checked.
-const LookupItemsMultiModStrideLength = 1
+const LookupItemsMultiModStrideLength = 32
 
 // IndexQuery represents a query running over established indices
 //
