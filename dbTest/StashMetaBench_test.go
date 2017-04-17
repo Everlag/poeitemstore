ff11207d9aa7b7244596121a22b42b0258f1f426
Everlag <kyleveb@gmail.com>
2017-04-16 23:46:38 -0600

    dbTest split BenchmarkAddStashesFast into Compact and AddStashes
    This allows us to determine which operation, compaction or adding
    stashes, should receive attention.

--- dbTest/StashMetaBench_test.go
+++ dbTest/StashMetaBench_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/Everlag/poeitemstore/db"
 )
 
-// BenchmarkAddStashesNoStringHeap determines how fast adding a single
-// response to the database is with an uninitialized StringHeap.
-func BenchmarkAddStashesNoStringHeapFast(b *testing.B) {
+// BenchmarkCompactAddStashesNoStringHeapFast determines how fast
+// adding a single response to the database is with an uninitialized StringHeap.
+func BenchmarkCompactAddStashesNoStringHeapFast(b *testing.B) {
 
 	// Fetch the changes we need
 	set := GetChangeSet("testSet - 11 updates.msgp", b)
@@ -45,9 +45,9 @@ func BenchmarkAddStashesNoStringHeapFast(b *testing.B) {
 
 }
 
-// BenchmarkAddStashes determines how fast adding a single
+// BenchmarkCompactAddStashesFast determines how fast adding a single
 // response to the database is with an already initialized StringHeap
-func BenchmarkAddStashesFast(b *testing.B) {
+func BenchmarkCompactAddStashesFast(b *testing.B) {
 
 	// Fetch the changes we need
 	set := GetChangeSet("testSet - 11 updates.msgp", b)
@@ -79,6 +79,89 @@ func BenchmarkAddStashesFast(b *testing.B) {
 		if err != nil {
 			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
 		}
+
+		_, err = db.AddStashes(cStashes, cItems, bdb)
+		if err != nil {
+			b.Fatalf("failed to AddStashes, err=%s", err)
+		}
+	}
+
+}
+
+// BenchmarkCompactFast determines how fast compacting a response
+// using an initialized StringHeap is.
+//
+// This should let us determine where improvements need to be made
+// in order to help BenchmarkCompactAddStashesFast
+func BenchmarkCompactFast(b *testing.B) {
+
+	// Fetch the changes we need
+	set := GetChangeSet("testSet - 11 updates.msgp", b)
+	change, err := set.GetFirstChange()
+	if err != nil {
+		b.Fatalf("failed to get first change")
+	}
+
+	resp, err := change.Decompress()
+	if err != nil {
+		b.Fatalf("failed to decompress response")
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		// Create a database and populate StringHeap, we want to exclude
+		// the time this takes from the benchmark
+		b.StopTimer()
+		bdb := NewTempDatabase(b)
+		_, _, err := db.StashStashToCompact(resp.Stashes, bdb)
+		if err != nil {
+			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
+		}
+		b.StartTimer()
+
+		_, _, err = db.StashStashToCompact(resp.Stashes, bdb)
+		if err != nil {
+			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
+		}
+	}
+
+}
+
+// BenchmarkCompactFast determines how fast adding a response to the database
+// is with an initialized StringHeap as well as already compacted items.
+//
+//
+// This should let us determine where improvements need to be made
+// in order to help BenchmarkCompactAddStashesFast
+func BenchmarkAddStashesFast(b *testing.B) {
+
+	// Fetch the changes we need
+	set := GetChangeSet("testSet - 11 updates.msgp", b)
+	change, err := set.GetFirstChange()
+	if err != nil {
+		b.Fatalf("failed to get first change")
+	}
+
+	resp, err := change.Decompress()
+	if err != nil {
+		b.Fatalf("failed to decompress response")
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		// Create a database and populate StringHeap, we want to exclude
+		// the time this takes from the benchmark
+		b.StopTimer()
+		bdb := NewTempDatabase(b)
+		cStashes, cItems, err := db.StashStashToCompact(resp.Stashes, bdb)
+		if err != nil {
+			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
+		}
+		b.StartTimer()
 
 		_, err = db.AddStashes(cStashes, cItems, bdb)
 		if err != nil {
