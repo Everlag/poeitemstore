b1962a5ef8f315616d4a87e055c7431ed9a1663e
Everlag <kyleveb@gmail.com>
2017-04-16 23:57:13 -0600

    dbTest BenchmarkCompactAddStashesNoStringHeapFast remove
    The vast majority of responses will always have StringHeap initialized,
    this benchmark is unnecessary. The name is also REALLY long.

--- dbTest/StashMetaBench_test.go
+++ dbTest/StashMetaBench_test.go
@@ -6,45 +6,6 @@ import (
 	"github.com/Everlag/poeitemstore/db"
 )
 
-// BenchmarkCompactAddStashesNoStringHeapFast determines how fast
-// adding a single response to the database is with an uninitialized StringHeap.
-func BenchmarkCompactAddStashesNoStringHeapFast(b *testing.B) {
-
-	// Fetch the changes we need
-	set := GetChangeSet("testSet - 11 updates.msgp", b)
-	change, err := set.GetFirstChange()
-	if err != nil {
-		b.Fatalf("failed to get first change")
-	}
-
-	resp, err := change.Decompress()
-	if err != nil {
-		b.Fatalf("failed to decompress response")
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		// Create a database, we want to exclude
-		// the time this takes from the benchmark
-		b.StopTimer()
-		bdb := NewTempDatabase(b)
-		b.StartTimer()
-
-		cStashes, cItems, err := db.StashStashToCompact(resp.Stashes, bdb)
-		if err != nil {
-			b.Fatalf("failed to convert fat stashes to compact, err=%s\n", err)
-		}
-
-		_, err = db.AddStashes(cStashes, cItems, bdb)
-		if err != nil {
-			b.Fatalf("failed to AddStashes, err=%s", err)
-		}
-	}
-
-}
-
 // BenchmarkCompactAddStashesFast determines how fast adding a single
 // response to the database is with an already initialized StringHeap
 func BenchmarkCompactAddStashesFast(b *testing.B) {
