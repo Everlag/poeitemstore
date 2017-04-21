02ae0aae77758c54a84ee8142d01a9f8b9217a32
Everlag <kyleveb@gmail.com>
2017-04-21 16:23:32 -0600

    db factor out Compact and Inflate from structs definitions
    These are logic that I'd prefer to keep centralized. structs.go was also
    getting quite crowded.

---
+++ db/compact.go
@@ -0,0 +1,186 @@
+package db
+
+import (
+	"github.com/Everlag/poeitemstore/stash"
+	"github.com/boltdb/bolt"
+	"github.com/pkg/errors"
+)
+
+// Inflate returns the string represented by the given StringHeapID
+func (id StringHeapID) Inflate(db *bolt.DB) string {
+	return InflateString(id, db)
+}
+
+// Inflate returns the string represented by the given LeagueHeapID
+func (id LeagueHeapID) Inflate(db *bolt.DB) string {
+	return InflateLeague(id, db)
+}
+
+// Inflate returns an inflated equivalent item modifier for human use
+func (mod ItemMod) Inflate(db *bolt.DB) stash.ItemMod {
+
+	return stash.ItemMod{
+		Template: []byte(mod.Mod.Inflate(db)),
+		Values:   mod.Values,
+	}
+
+}
+
+// Inflate returns an inflated equivalent item fit for human use
+func (item Item) Inflate(db *bolt.DB) stash.Item {
+
+	// Initialize with the most trivial portions
+	fat := stash.Item{
+		Name:       item.Name.Inflate(db),
+		TypeLine:   item.TypeLine.Inflate(db),
+		Note:       item.Note.Inflate(db),
+		RootType:   item.RootType.Inflate(db),
+		RootFlavor: item.RootFlavor.Inflate(db),
+		League:     item.League.Inflate(db),
+		Corrupted:  item.Corrupted,
+		Identified: item.Identified,
+	}
+
+	// And the modifiers
+	fatMods := make([]stash.ItemMod, len(item.Mods))
+	for i, mod := range item.Mods {
+		fatMods[i] = mod.Inflate(db)
+	}
+	// Uhhhhh.... yeah okay, I don't really care about this.
+	// TODO: rethink choices
+	fat.ExplicitMods = fatMods
+
+	return fat
+}
+
+// StashItemsToCompact converts fat Item records to their compact form
+//
+// This also ensures all strings present on that item will be available
+// on the StringHeap
+func StashItemsToCompact(items []stash.Item, when Timestamp,
+	db *bolt.DB) ([]Item, error) {
+
+	compact := make([]Item, len(items))
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		// Translate leagues on a per-item basis
+		leagues := make([]string, len(items))
+		for i, item := range items {
+			leagues[i] = item.League
+		}
+		leagueIds, err := setLeagues(leagues, tx)
+		if err != nil {
+			return errors.Wrap(err, "failed to add leagues to LeagueHeap")
+		}
+
+		// Build compact items from the ids and fill in non-StringHeap information
+		for i, item := range items {
+			compact[i] = Item{
+				ID:             ID{}, // Explicitly empty on entrance
+				GGGID:          GGGIDFromUID(item.ID),
+				Stash:          GGGIDFromUID(item.StashID),
+				League:         leagueIds[i],
+				Identified:     item.Identified,
+				Corrupted:      item.Corrupted,
+				UpdateSequence: uint16(i),
+				When:           when,
+			}
+		}
+
+		// Populate StringHeap related information
+		if err := setStringsForItems(items, compact, tx); err != nil {
+			return errors.New("failed to set strings")
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to translate item to db form")
+	}
+
+	// Then add populate internal IDs, adding them as necessary
+	//
+	// Left outside the above transaction due to benchmarking results.
+	err = GetTranslations(compact, db)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to add internal IDs to items")
+	}
+
+	return compact, nil
+
+}
+
+// StashStashToCompact converts fat Item records to their compact form
+// while also stripping items out in their compact form.
+func StashStashToCompact(stashes []stash.Stash,
+	db *bolt.DB) ([]Stash, [][]Item, error) {
+
+	// Grab a new timestamp, all of the Stashes will share the same time
+	when := NewTimestamp()
+
+	// Compact stashes and flatten items
+	compact := make([]Stash, len(stashes))[:0] // Sliced to zero to allow append
+	flatItems := make([]stash.Item, 0)
+	// Keep track of per-stash leagues
+	leagues := make([]string, len(stashes))[:0]
+	// Keep track of items per-stash so we can unflatten them into per-stash
+	// item sets
+	itemsPerStash := make([]int, len(stashes))[:0]
+	for _, stash := range stashes {
+		// We skip empty stashes as we will be unable to assign
+		// then with a LeagueHeapID
+		if len(stash.Items) == 0 {
+			continue
+		}
+
+		compactStash := Stash{
+			AccountName: stash.AccountName,
+			ID:          GGGIDFromUID(stash.ID),
+		}
+
+		// Populate GGGIDs in this Stash
+		ids := make([]GGGID, len(stash.Items))
+		for i, item := range stash.Items {
+			ids[i] = GGGIDFromUID(item.ID)
+		}
+		compactStash.Items = ids
+
+		compact = append(compact, compactStash)
+
+		// Note the league for this Stash
+		leagues = append(leagues, stash.Items[0].League)
+		// Note the number of items included in this stash
+		itemsPerStash = append(itemsPerStash, len(stash.Items))
+
+		flatItems = append(flatItems, stash.Items...)
+	}
+
+	// Fetch and decorate the ids to the compact stashes
+	leagueIDs, err := SetLeagues(leagues, db)
+	if err != nil {
+		err = errors.Wrap(err, "failed to add LeagueHeapIDs to stashes")
+		return nil, nil, err
+	}
+	for i, id := range leagueIDs {
+		compact[i].League = id
+	}
+
+	// Grab the compact items as their flat form
+	compactItems, err := StashItemsToCompact(flatItems, when, db)
+	if err != nil {
+		err = errors.Wrap(err, "failed to compact items")
+		return nil, nil, err
+	}
+
+	// Unflatten the items so they can match an associated stash
+	items := make([][]Item, len(leagueIDs)) // Sized exactly using leagueIDs
+	lastItemBase := 0
+	for i, count := range itemsPerStash {
+		itemGroup := compactItems[lastItemBase : lastItemBase+count]
+		items[i] = itemGroup
+		// items[i] = compactItems[lastItemBase:count]
+		lastItemBase += count
+	}
+
+	return compact, items, nil
+
+}
--- db/structs.go
+++ db/structs.go
@@ -5,10 +5,7 @@ package db
 import (
 	"time"
 
-	"github.com/Everlag/poeitemstore/stash"
-	"github.com/boltdb/bolt"
 	blake2b "github.com/minio/blake2b-simd"
-	"github.com/pkg/errors"
 )
 
 // StringHeapID maps to a stored string identifier.
@@ -26,11 +23,6 @@ func StringHeapIDFromBytes(bytes []byte) StringHeapID {
 // ToBytes returns the byte-wise represenation of a StringHeapID
 func (id StringHeapID) ToBytes() []byte {
 	return i32tob(uint32(id))
-}
-
-// Inflate returns the string represented by the given StringHeapID
-func (id StringHeapID) Inflate(db *bolt.DB) string {
-	return InflateString(id, db)
 }
 
 // LeagueHeapID maps to a stored string identifier specific to league
@@ -53,11 +45,6 @@ func LeagueHeapIDFromBytes(bytes []byte) LeagueHeapID {
 // ToBytes returns the byte-wise represenation of a LeagueHeapID
 func (id LeagueHeapID) ToBytes() []byte {
 	return i16tob(uint16(id))
-}
-
-// Inflate returns the string represented by the given LeagueHeapID
-func (id LeagueHeapID) Inflate(db *bolt.DB) string {
-	return InflateLeague(id, db)
 }
 
 // TimestampSize is the number of bytes used by Timestamp
@@ -136,16 +123,6 @@ type ItemMod struct {
 	Values []uint16
 }
 
-// Inflate returns an inflated equivalent item modifier for human use
-func (mod ItemMod) Inflate(db *bolt.DB) stash.ItemMod {
-
-	return stash.ItemMod{
-		Template: []byte(mod.Mod.Inflate(db)),
-		Values:   mod.Values,
-	}
-
-}
-
 // Item represents a compact record of an item.
 //msgp:tuple Item
 type Item struct {
@@ -163,89 +140,6 @@ type Item struct {
 	Mods           []ItemMod
 	When           Timestamp // When this stash update was processed
 	UpdateSequence uint16    // The sequence number associated with this item
-}
-
-// Inflate returns an inflated equivalent item fit for human use
-func (item Item) Inflate(db *bolt.DB) stash.Item {
-
-	// Initialize with the most trivial portions
-	fat := stash.Item{
-		Name:       item.Name.Inflate(db),
-		TypeLine:   item.TypeLine.Inflate(db),
-		Note:       item.Note.Inflate(db),
-		RootType:   item.RootType.Inflate(db),
-		RootFlavor: item.RootFlavor.Inflate(db),
-		League:     item.League.Inflate(db),
-		Corrupted:  item.Corrupted,
-		Identified: item.Identified,
-	}
-
-	// And the modifiers
-	fatMods := make([]stash.ItemMod, len(item.Mods))
-	for i, mod := range item.Mods {
-		fatMods[i] = mod.Inflate(db)
-	}
-	// Uhhhhh.... yeah okay, I don't really care about this.
-	// TODO: rethink choices
-	fat.ExplicitMods = fatMods
-
-	return fat
-}
-
-// StashItemsToCompact converts fat Item records to their compact form
-//
-// This also ensures all strings present on that item will be available
-// on the StringHeap
-func StashItemsToCompact(items []stash.Item, when Timestamp,
-	db *bolt.DB) ([]Item, error) {
-
-	compact := make([]Item, len(items))
-
-	err := db.Update(func(tx *bolt.Tx) error {
-		// Translate leagues on a per-item basis
-		leagues := make([]string, len(items))
-		for i, item := range items {
-			leagues[i] = item.League
-		}
-		leagueIds, err := setLeagues(leagues, tx)
-		if err != nil {
-			return errors.Wrap(err, "failed to add leagues to LeagueHeap")
-		}
-
-		// Build compact items from the ids and fill in non-StringHeap information
-		for i, item := range items {
-			compact[i] = Item{
-				ID:             ID{}, // Explicitly empty on entrance
-				GGGID:          GGGIDFromUID(item.ID),
-				Stash:          GGGIDFromUID(item.StashID),
-				League:         leagueIds[i],
-				Identified:     item.Identified,
-				Corrupted:      item.Corrupted,
-				UpdateSequence: uint16(i),
-				When:           when,
-			}
-		}
-
-		// Populate StringHeap related information
-		if err := setStringsForItems(items, compact, tx); err != nil {
-			return errors.New("failed to set strings")
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to translate item to db form")
-	}
-
-	// Then add populate internal IDs, adding them as necessary
-	//
-	// Left outside the above transaction due to benchmarking results.
-	err = GetTranslations(compact, db)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to add internal IDs to items")
-	}
-
-	return compact, nil
-
 }
 
 // Stash represents a compact record of a stash.
@@ -291,80 +185,4 @@ func (s Stash) Diff(old Stash) (add, remove []GGGID) {
 	}
 
 	return
-}
-
-// StashStashToCompact converts fat Item records to their compact form
-// while also stripping items out in their compact form.
-func StashStashToCompact(stashes []stash.Stash,
-	db *bolt.DB) ([]Stash, [][]Item, error) {
-
-	// Grab a new timestamp, all of the Stashes will share the same time
-	when := NewTimestamp()
-
-	// Compact stashes and flatten items
-	compact := make([]Stash, len(stashes))[:0] // Sliced to zero to allow append
-	flatItems := make([]stash.Item, 0)
-	// Keep track of per-stash leagues
-	leagues := make([]string, len(stashes))[:0]
-	// Keep track of items per-stash so we can unflatten them into per-stash
-	// item sets
-	itemsPerStash := make([]int, len(stashes))[:0]
-	for _, stash := range stashes {
-		// We skip empty stashes as we will be unable to assign
-		// then with a LeagueHeapID
-		if len(stash.Items) == 0 {
-			continue
-		}
-
-		compactStash := Stash{
-			AccountName: stash.AccountName,
-			ID:          GGGIDFromUID(stash.ID),
-		}
-
-		// Populate GGGIDs in this Stash
-		ids := make([]GGGID, len(stash.Items))
-		for i, item := range stash.Items {
-			ids[i] = GGGIDFromUID(item.ID)
-		}
-		compactStash.Items = ids
-
-		compact = append(compact, compactStash)
-
-		// Note the league for this Stash
-		leagues = append(leagues, stash.Items[0].League)
-		// Note the number of items included in this stash
-		itemsPerStash = append(itemsPerStash, len(stash.Items))
-
-		flatItems = append(flatItems, stash.Items...)
-	}
-
-	// Fetch and decorate the ids to the compact stashes
-	leagueIDs, err := SetLeagues(leagues, db)
-	if err != nil {
-		err = errors.Wrap(err, "failed to add LeagueHeapIDs to stashes")
-		return nil, nil, err
-	}
-	for i, id := range leagueIDs {
-		compact[i].League = id
-	}
-
-	// Grab the compact items as their flat form
-	compactItems, err := StashItemsToCompact(flatItems, when, db)
-	if err != nil {
-		err = errors.Wrap(err, "failed to compact items")
-		return nil, nil, err
-	}
-
-	// Unflatten the items so they can match an associated stash
-	items := make([][]Item, len(leagueIDs)) // Sized exactly using leagueIDs
-	lastItemBase := 0
-	for i, count := range itemsPerStash {
-		itemGroup := compactItems[lastItemBase : lastItemBase+count]
-		items[i] = itemGroup
-		// items[i] = compactItems[lastItemBase:count]
-		lastItemBase += count
-	}
-
-	return compact, items, nil
-
 }
